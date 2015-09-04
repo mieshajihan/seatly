3dfe8561f6224b4a9ecd47adc2209ea9053a9a84
companyhen <companyhen@gmail.com>
2015-09-04 16:58:35 -0400

    adding button toggle to display form in js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// alert('Welcome to Seatly!');
+function toggleForm(id) {
+   var e = document.getElementById("reservationForm");
+   if(e.style.display == 'block') {
+      e.style.display = 'none';
+   }
+   else {
+      e.style.display = 'block';
+   }
+}
+
+// var reservationForm = document.getElementById('reservationForm');
+
+// document.querySelector(".seat").addEventListener('click', reserveSeat);
+
+// function reserveSeat() {
+// 	document.querySelector(".seat").addEventListener("click", function(){
+// 	    document.querySelector("#reservationForm").style.display = "block";
+// 	});	
+// }
+
+// reserveSeat();
+
+// var seat = document.getElementsByClassName('seat');
+
+// function showForm(seats) {
+// 	seats.forEach(function(){
+// 		document.querySelector(".seat").addEventListener("click", function(){
+// 	    	document.querySelector("#reservationForm").style.display = "block";
+// 		});	
+// 	});
+// }
+
+// showForm();
