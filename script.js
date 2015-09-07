71fed98eebb79d52a29dd2bb479a0a5c8446e851
companyhen <companyhen@gmail.com>
2015-09-06 20:56:40 -0400

    restructuring some code

--- script.js
+++ script.js
@@ -2,41 +2,48 @@
 var seats = [];
 var numOfSeats = 24;
 for (i = 1; i <= numOfSeats; i++) {
-	seats.push({ seat: i, reserved: false } );
+	seats.push({ seat: i, reserved: false });
 }
 
 // Variables
-var seatsContainer = document.getElementById('seats');
 var seatBtn = document.getElementsByClassName('seat');
-var form    = document.getElementById('reservationForm');
+var form  = document.getElementById('reservationForm');
 var activeSeat = document.getElementById('activeSeat');
 var submit = document.getElementById('submit');
 
 // Event handlers
-// $('.seat').on('click', showForm);
 for(i=0; i<seatBtn.length; i++){
 	seatBtn[i].addEventListener('click', showForm);
 }
 submit.addEventListener('click', submitForm);
 
 // Functions
+
+// attempting to add seat # HTML..
+// function getSeatNumber(el) {
+// 	el.forEach(function(seat) {
+// 		console.log(seat.seat);
+// 	});
+// }
+// getSeatNumber(seats);
+
 function showForm() {
   form.style.display = 'block';
 
   $('.seat').addClass('btn-inactive');
-  $(this).removeClass('btn-inactive');
-  $(this).addClass('btn-primary active');
+  $(this).removeClass('btn-inactive').addClass('btn-primary active');
+  
+  var seat = $(this).data('seat');
+  $('#currentSeat').val(seat);
+  $('#activeSeat').text(seat);
 };
 
 function submitForm(e) {
 	e.preventDefault();
 
 	$('.seat').removeClass('btn-inactive'); 
-	$('.active').addClass('btn-danger');
+	$('.active').addClass('btn-danger btn-inactive').html('Reserved');
 	form.style.display = 'none';
-
-	$('.active').html('Reserved');
 
 	form.reset();
 };
-
